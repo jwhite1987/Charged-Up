9f3c7c0f9b6e43963860b2ad447daaf8a1a46222
Jimmy <>
2021-05-01 14:46:21 -0400

    new

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 
-client = pymongo.MongoClient("mongodb+srv://jimmywhite87:Ruger2012!?@charged-up.yt7ie.mongodb.net/charged-up?retryWrites=true&w=majority")
+client = PyMongo.MongoClient("mongodb+srv://jimmywhite87:Ruger2012!?@charged-up.yt7ie.mongodb.net/charged-up?retryWrites=true&w=majority")
 db = client.test
 
 # Create an instance of Flask
