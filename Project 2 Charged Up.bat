28eec8c7cb07fc98a896d634982b5b72b7c4f488
Mariam Ahmad <Mahmad142002@yahoo.com>
2021-05-03 12:44:09 -0400

    Add files via upload

---
+++ Project 2 Charged Up.bat
@@ -0,0 +1,29 @@
+Project title: Visualization Project: Charged Up
+
+Team Members: Zack Rogers, Jimmy White, Akanksha Kukhraniya, Mariam Ahmad
+
+Projcet Description: The team will take EV (Electric Vehicle) Charging station data and show the efficiences of 
+different EV model would perform traveling across the country
+
+Data Sets to be used: Kaggle 
+Data set name: EVs-One electric Vehicle Dataset
+File type: .csv
+Source: : Supercharge.info/data
+Dataset Name: Supercharge info
+File Type: .json
+
+Break down of task: Topic idea generation
+Documentation of project proposal
+Manage GitHub 
+Create html and json files
+Pull data from json & csv datasets 
+Use D3 to show visualizations 
+Plot map markers showing each charging station 
+Create analysis for each datasets using PostgreSQL
+
+
+Introduction: We decided to pick a subject of a current trend, owning an EV. With the world shifting
+towards a more sustainable and environmentally friendly way of living we wanted to do a breakdown of how 
+practical owning an electric vehicle would be with infastructure supporting it today. We will discuss which 
+vehicles are most efficient, have the most range, and areas that would most likely benefit you owning an 
+electric vehicle. 
